066d52af93142b8ef1e1843235a527df10d29a0f
Evan Placenis <placenie@mcmaster.ca>
2022-09-12 18:52:34 -0400

    Updates

---
+++ precache-manifest.6a881ec139f6aa7d193893c6f00dc270.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d808007f28ca36779aa5",
+    "url": "/RoboFriendsWeb/static/css/main.880f0f6d.chunk.css"
+  },
+  {
+    "revision": "d808007f28ca36779aa5",
+    "url": "/RoboFriendsWeb/static/js/main.d808007f.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/RoboFriendsWeb/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "5bcf2286dd13a7b2f0c3",
+    "url": "/RoboFriendsWeb/static/css/2.3af3a6dc.chunk.css"
+  },
+  {
+    "revision": "5bcf2286dd13a7b2f0c3",
+    "url": "/RoboFriendsWeb/static/js/2.5bcf2286.chunk.js"
+  },
+  {
+    "revision": "1210131f9ef82cf94e9a",
+    "url": "/RoboFriendsWeb/static/js/runtime~main.1210131f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriendsWeb/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "7d1cca02098fc07ffffd352b73105e4d",
+    "url": "/RoboFriendsWeb/index.html"
+  }
+];
